18b44133ee912c3048bd93d71641935c4327a307
romainniamor <romainnavoret@gmail.com>
2024-01-13 21:10:24 +0100

    ♻️  F04: ORDER > create NavBar & Main

--- src/components/pages/order/orderPage.jsx
+++ src/components/pages/order/orderPage.jsx
@@ -1,18 +1,40 @@
 import { Link, useParams } from "react-router-dom";
+import { theme } from "../../../theme";
+import styled from "styled-components";
+import NavBar from "./NavBar";
+import Main from "./Main";
 
 export default function OrderPage() {
   //state
-  const { userName } = useParams();
 
   //comportements
 
   //render
   return (
-    <>
-      <h1>Bonjour {userName}</h1>
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link>
-    </>
+    <OrderPageStyled>
+      <div className="container">
+        <NavBar></NavBar>
+        <Main />
+      </div>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${theme.colors.primary};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    max-width: 1400px;
+    width: 95%;
+    height: 95%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    border-radius: 15px;
+  }
+`;
