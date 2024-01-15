c4c2deb3e48a3558e66c6829d6d4c0d2dd425c4e
romainniamor <romainnavoret@gmail.com>
2024-01-15 11:22:35 +0100

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,11 +2,17 @@ import styled from "styled-components";
 import { theme } from "../../../theme/index";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket"></div>
+      <div className="menu"></div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.main`
   flex: 1;
   background: ${theme.colors.background_white};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  display: grid;
+  grid-template-columns: 25% 75%;
 `;
