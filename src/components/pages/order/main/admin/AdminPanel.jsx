efbc6d7705a3b31faae7a30d5920eb8ac8c581d2
romainniamor <romainnavoret@gmail.com>
2024-01-19 23:37:39 +0100

    ♻️ F07: ADMIN > Refact AdminPanel conditionnal render

--- src/components/pages/order/main/admin/AdminPanel.jsx
+++ src/components/pages/order/main/admin/AdminPanel.jsx
@@ -2,16 +2,23 @@ import { useContext } from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../theme/index";
 import OrderContext from "../../../../../contexts/orderContext";
+import { getTabsConfig } from "./getTabsConfig";
 
 export default function AdminPanel() {
-  const { isAddTabSelected, isEditTabSelected } = useContext(OrderContext);
+  const { isAddTabSelected, isEditTabSelected, currentTabSelected } =
+    useContext(OrderContext);
+
+  const tabs = getTabsConfig(currentTabSelected);
   return (
     <AdminPanelStyled>
-      {isAddTabSelected ? (
+      {currentTabSelected === "add" && <PanelEdit />}
+      {currentTabSelected === "edit" && <div>Editer un produit</div>}
+
+      {/* {isAddTabSelected ? (
         <div>Ajouter un produit</div>
       ) : (
         <div>Editer un produit</div>
-      )}
+      )} */}
     </AdminPanelStyled>
   );
 }
@@ -22,3 +29,15 @@ const AdminPanelStyled = styled.div`
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
 `;
+
+function PanelEdit() {
+  return (
+    <div>
+      <h1>Panel edit</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse,
+        corporis.
+      </p>
+    </div>
+  );
+}
