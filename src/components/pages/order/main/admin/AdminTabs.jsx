cc07c98f0a76e0158328222942d0a62113fc9c4d
romainniamor <romainnavoret@gmail.com>
2024-01-19 10:12:35 +0100

    ✨ F07: ADMIN > Tab > Add label

--- src/components/pages/order/main/admin/AdminTabs.jsx
+++ src/components/pages/order/main/admin/AdminTabs.jsx
@@ -3,25 +3,50 @@ import styled from "styled-components";
 import { theme } from "../../../../../theme/index";
 import Tab from "../../../../reusableUi/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
+  //state
+  const [isActive, setIsActive] = useState(true);
+
+  //comportements
   const togggleIsCollapsed = () => {
     setIsCollapsed(!isCollapsed);
+  };
+
+  const toggleIsActive = () => {
+    setIsActive(!isActive);
   };
 
   return (
     <AdminTabsStyled>
       <Tab
         icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
-        className={isCollapsed ? "collapsed" : ""}
+        className={isCollapsed ? "active" : ""}
         onClick={togggleIsCollapsed}
+      />
+      <Tab
+        icon={<AiOutlinePlus />}
+        label={"Ajouter un produit"}
+        className={isActive ? "active" : ""}
+        onClick={toggleIsActive}
+      />
+      <Tab
+        icon={<MdModeEditOutline />}
+        label={"Editer un produit"}
+        className={isActive ? "" : "active"}
+        onClick={toggleIsActive}
       />
     </AdminTabsStyled>
   );
 }
 
 const AdminTabsStyled = styled.div`
-  .collapsed {
+  display: flex;
+  gap: 1px;
+  padding: 0 20px;
+  .active {
     background-color: ${theme.colors.background_dark};
     color: ${theme.colors.white};
     border-color: ${theme.colors.background_dark};
