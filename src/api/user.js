780c9276516673ad9a8c26fa10aadfcf21626008
romainniamor <romainnavoret@gmail.com>
2024-02-01 14:56:41 +0100

    ✨ F13:USER > add authenticate  user

--- src/api/user.js
+++ src/api/user.js
@@ -2,21 +2,28 @@ import { getDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "./firebase-config";
 import { fakeMenu as menu } from "../fakeData/fakeMenu";
 
-export const getUser = async (idUser) => {
-  const docRef = doc(db, "users", idUser);
+const getUser = async (userId) => {
+  const docRef = doc(db, "users", userId);
   const docSnapshot = await getDoc(docRef);
   if (docSnapshot.exists()) {
-    //  true or false
-    const data = docSnapshot.data();
-    return data; // return data user
+    const userReceived = docSnapshot.data();
+    return userReceived;
   }
 };
 
-export const createUser = (userId) => {
+const createUser = (userId) => {
   const docRef = doc(db, "users", userId); //chemin
   const data = {
     username: userId,
-    menu: menu.LARGE, //des données
+    menu: menu.SMALL, //des données
   };
   setDoc(docRef, data);
+};
+
+export const authenticateUser = async (userId) => {
+  const existingUser = await getUser(userId);
+  if (!existingUser) {
+    createUser(userId);
+  }
+  return existingUser;
 };
